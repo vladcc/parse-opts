65e83c56a0da2a574e52672d3d82aafa99e4dd92
Vladimir <vld.dinev@gmail.com>
2019-09-12 00:11:37 +0200

    Documented, tested TODO: review and upload; update date if something changed after the review

---
+++ main.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include "parse_opts.h"
+
+static void hnd_flag_i(char * opt, char * arg, void * p)
+{
+    printf("%s\n", opt);
+}
+
+static void hlp_flag_i(char * short_name, char * long_name)
+{
+    printf("help for flag %s, %s\n", short_name, long_name);
+}
+
+static void hnd_flag_j(char * opt, char * arg, void * p)
+{
+    printf("%s\n", opt);
+}
+
+static void hlp_flag_j(char * short_name, char * long_name)
+{
+    printf("help for flag %s, %s\n", short_name, long_name);
+}
+
+static void hnd_flag_k(char * opt, char * arg, void * p)
+{
+    printf("%s\n", opt);
+}
+
+static void hlp_flag_k(char * short_name, char * long_name)
+{
+    printf("help for flag %s, %s\n", short_name, long_name);
+}
+
+static void hnd_a(char * opt, char * arg, void * p)
+{
+    printf("%s %s\n", opt, arg);
+}
+
+static void hlp_a(char * short_name, char * long_name)
+{
+    printf("help for option %s, %s\n", short_name, long_name);
+}
+
+static void hnd_help(char * opt, char * arg, void * p)
+{
+    opts_print_help(p);
+}
+
+static void hlp_help(char * short_name, char * long_name)
+{
+    printf("help for option %s, %s\n", short_name, long_name);
+}
+
+static void hnd_s_args(char * opt, char * arg, void * p)
+{
+    printf("%s ", opt);
+    char * sub_arg, ** parg = &arg;
+    while ((sub_arg = opts_get_sub_arg(parg, ',')))
+        printf("'%s' ", sub_arg);
+    putchar('\n');
+}
+
+static void hlp_s_args(char * short_name, char * long_name)
+{
+    printf("help for option %s, %s\n", short_name, long_name);
+}
+
+
+static void unknown(char * opt, char * arg, void * p)
+{
+    printf("unknown option: '%s'\n", opt);
+}
+
+static void wild_arg(char * opt, char * arg, void * p)
+{
+    printf("arg without option: %s\n", arg);
+}
+
+int main(int argc, char * argv[])
+{
+    if (argc < 2)
+    {
+        puts("Use: <prog> <options>");
+        return -1;
+    }
+
+    opts_table the_tbl;
+    opts_entry all_entries[] = {
+        {.short_name = 'i', .long_name = "i-flag", .takes_arg = false,
+         .callback = hnd_flag_i, .callback_arg = NULL,
+         .print_help = hlp_flag_i},
+        {.short_name = 'j', .long_name = "j-flag", .takes_arg = false,
+         .callback = hnd_flag_j, .callback_arg = NULL,
+         .print_help = hlp_flag_j},
+        {.short_name = 'k', .long_name = "k-flag", .takes_arg = false,
+         .callback = hnd_flag_k, .callback_arg = NULL,
+         .print_help = hlp_flag_k},
+        {.short_name = 'a', .long_name = "a-option", .takes_arg = true,
+         .callback = hnd_a, .callback_arg = NULL,
+         .print_help = hlp_a},
+        {.short_name = 'h', .long_name = "help", .takes_arg = false,
+         .callback = hnd_help, .callback_arg = &the_tbl,
+         .print_help = hlp_help},
+        {.short_name = 's', .long_name = "sub-args", .takes_arg = true,
+         .callback = hnd_s_args, .callback_arg = NULL,
+         .print_help = hlp_s_args},
+    };
+
+    the_tbl.tbl = all_entries;
+    the_tbl.length = sizeof(all_entries)/sizeof(*all_entries);
+
+    opts_parse(argc-1, argv+1, &the_tbl, wild_arg, NULL, unknown);
+    return 0;
+}
---
+++ run_tests.sh
@@ -0,0 +1,86 @@
+G_TMP_FILE="tmp_result.txt"
+G_BINARY=""
+
+function run_write
+{
+    local LOC_TCASE="$1"
+    $G_BINARY $LOC_TCASE > $G_TMP_FILE 2>&1
+}
+
+function run_append
+{
+    local LOC_TCASE="$1"
+    $G_BINARY $LOC_TCASE >> $G_TMP_FILE 2>&1
+}
+
+function file_compare
+{
+    local LOC_TFILE="$1"
+    local LOC_TCASE="$2"
+
+    diff $G_TMP_FILE $LOC_TFILE
+    if [ $? -ne 0 ]; then
+        echo "Failed test case:" $LOC_TCASE
+        echo "Result with file" $LOC_TFILE "differ"
+        exit 1
+    fi
+}
+
+function test_run_compare
+{
+    local LOC_TCASE="$1"
+    local LOC_TFILE="$2"
+    run_write "$LOC_TCASE"
+    file_compare "$LOC_TFILE" "$LOC_TCASE"
+}
+
+function main
+{
+    if [ "$#" -ne 1 ]; then
+        echo "Use: $0 <path-to-opts-test-binary>"
+        exit 1
+    fi
+
+    G_BINARY="$1"
+    TEST_DIR="./tests"
+
+    TEST_FILE="$TEST_DIR/short_names.txt"
+    TEST_CASE="-i -j -k -kji -jkia123 -jkia 123 -akji -ks1,2,3"
+    test_run_compare "$TEST_CASE" "$TEST_FILE"
+
+    TEST_FILE="$TEST_DIR/long_names.txt"
+    TEST_CASE="--i-flag --a-option a,b,c --sub-args a,b,c"
+    test_run_compare "$TEST_CASE" "$TEST_FILE"
+
+    TEST_FILE="$TEST_DIR/requires_arg_err.txt"
+    run_write "-a"
+    run_append "--a-option"
+    run_append "--a-option="
+    file_compare $TEST_FILE "-a, then --a-option, then --a-option="
+
+    TEST_FILE="$TEST_DIR/flag_no_arg_err.txt"
+    TEST_CASE="-ibcd -i bcd --i-flag bcd"
+    run_write "$TEST_CASE"
+    run_append "--i-flag=bcd"
+    run_append "--i-flag="
+    file_compare $TEST_FILE "$TEST_CASE, then --i-flag=bcd, then --i-flag="
+
+    TEST_FILE="$TEST_DIR/args_sub_args.txt"
+    TEST_CASE="-sa,b,c -s,,,,a,,b,c,,, --sub-args=a,b,c --sub-args=,,a,,b,,,c,,,,
+    --sub-args a,b,c --sub-args ,,a,,,b,,,c, -sa --sub-args=a --sub-args a
+    --sub-args= ,a,,,b,,,c --sub-args= a,b,c -aa,b,c  --a-option=a,b,c
+    --a-option= a,b,c --a-option a,b,c"
+    test_run_compare "$TEST_CASE" "$TEST_FILE"
+
+    TEST_FILE="$TEST_DIR/no_opt_args_dash_and_all_is_arg.txt"
+    TEST_CASE="-ij one -k two three -a123 - -- -a123 --sub-args"
+    test_run_compare "$TEST_CASE" "$TEST_FILE"
+
+    TEST_FILE="$TEST_DIR/unknown_opt_and_help.txt"
+    TEST_CASE="-ijzmk --a-option 123 --random --help -z -h"
+    test_run_compare "$TEST_CASE" "$TEST_FILE"
+
+    rm $G_TMP_FILE
+}
+
+main $@
